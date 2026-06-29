63650aaed55681550b41e0831902eee65d98fbef
rare-aniket <aniket@rarepixelsdesign.com>
2026-06-29 14:23:50 +0530

    test 🧪:  Check 2 Added Aria-lable to button of Testimonials to Improve Robot Indexing

--- src/components/Homepage/Mobile/TestimonialSectionMobile.tsx
+++ src/components/Homepage/Mobile/TestimonialSectionMobile.tsx
@@ -170,6 +170,7 @@ export default function TestimonialSectionMobile() {
                                 role="button"
                                 tabIndex={0}
                                 onClick={() => swiperRef.current?.slidePrev()}
+                                aria-label="Previous Testimonial"
                             >
                                 <svg className="testimonial-svg" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <mask id="mask0_2178_856" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
@@ -185,6 +186,7 @@ export default function TestimonialSectionMobile() {
                                 role="button"
                                 tabIndex={0}
                                 onClick={() => swiperRef.current?.slideNext()}
+                                aria-label="Next Testimonial"
                             >
                                 <svg className="testimonial-svg" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <mask id="mask0_2178_853" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
