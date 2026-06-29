3fe255d63a82759ee2f58f82eb3e44f0cbf42233
rare-aniket <aniket@rarepixelsdesign.com>
2026-06-29 14:32:12 +0530

    test 🧪: Check 3 Added Hero Section in Dexktop Screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
 					:
 					<>
 						{/* DESKTOP COMPONENTS */}
-						{/* <HeroSectionDesktop /> */}
+						<HeroSectionDesktop />
 						<TestimonialSectionDesktop />
 						<AboutSectionDesktop />
 						<ServicesSectionDesktop />
