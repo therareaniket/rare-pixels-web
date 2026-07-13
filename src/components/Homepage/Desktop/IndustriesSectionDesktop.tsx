09305ad40992d96c278d6bfca376dcd8b418bc58
rajvi16-rarepixels <rajvi.rarepixelsdesign@gmail.com>
2026-07-13 14:44:04 +0530

    test 🧪:  working on industries section

--- src/components/Homepage/Desktop/IndustriesSectionDesktop.tsx
+++ src/components/Homepage/Desktop/IndustriesSectionDesktop.tsx
@@ -1,5 +1,63 @@
 'use client';
 
+
+const industries = [
+    {
+        video: "/images/homepage/industries/technology-and-saas.mp4",
+        title: "Technology & SaaS",
+        desc1: "Complex products fail when users don't understand them.",
+        desc2: "We transform powerful SaaS platforms into intuitive experiences that drive adoption, retention, and growth.",
+    },
+
+    {
+        video: "",
+        title: "Finance & FinTech",
+        desc1: "Trust is the product before the product.",
+        desc2: "We create secure, credible, and intuitive financial experiences that help users transact with confidence.",
+    },
+
+    {
+        video: "",
+        title: "Healthcare & MedTech",
+        desc1: "Every second matters when people seek care.",
+        desc2: "We design healthcare experiences that make information accessible, decisions easier, and journeys less stressful.",
+    },
+
+    {
+        video: "",
+        title: "E-commerce & Retail",
+        desc1: "Customers don't buy products. They buy experiences.",
+        desc2: "We build shopping journeys that reduce hesitation, increase conversions, and encourage repeat purchases.",
+    },
+
+    {
+        video: "",
+        title: "Real Estate & PropTech",
+        desc1: "People invest in confidence before they invest in property.",
+        desc2: "We help real estate brands create digital experiences that build trust long before a site visit.",
+    },
+
+    {
+        video: "",
+        title: "Education & EdTech",
+        desc1: "The best learning experiences never feel complicated.",
+        desc2: "We create intuitive platforms that keep students focused on learning, not figuring out how things work.",
+    },
+
+    {
+        video: "",
+        title: "AI & Emerging Tech",
+        desc1: "Innovation means little if people can't understand it.",
+        desc2: "We humanize emerging technologies through experiences that make complex products easier to adopt and trust.",
+    },
+
+    {
+        video: "",
+        title: "Food & Lifestyle",
+        desc1: "People remember how brands make them feel.",
+        desc2: "",
+    }
+]
 export default function IndustriesSectionDesktop() {
     return (
         <>
@@ -40,38 +98,10 @@ export default function IndustriesSectionDesktop() {
 
                             <div className="industries-text-wrapper">
                                 <span className="text-sb industries-title-name">Technology & SaaS</span>
-                                {/* <span className="text-sb industries-title-name">Finance & FinTech</span> */}
-                                {/* <span className="text-sb industries-title-name">Healthcare & MedTech</span> */}
-                                {/* <span className="text-sb industries-title-name">E-commerce & Retail</span> */}
-                                {/* <span className="text-sb industries-title-name">Real Estate & PropTech</span> */}
-                                {/* <span className="text-sb industries-title-name">Education & EdTech</span> */}
-                                {/* <span className="text-sb industries-title-name">AI & Emerging Tech</span> */}
-                                {/* <span className="text-sb industries-title-name">Food & Lifestyle</span> */}
 
                                 <p className="text-18 text-grey">
                                     <span className="text-rg">Complex products fail when users don&apos;t understand them.</span>
                                     <span className="text-rg">We transform powerful SaaS platforms into intuitive experiences that drive adoption, retention, and growth.</span>
-
-                                    {/* <span className="text-rg">Trust is the product before the product.</span>
-                                    <span className="text-rg">We create secure, credible, and intuitive financial experiences that help users transact with confidence.</span> */}
-
-                                    {/* <span className="text-rg">Every second matters when people seek care.</span>
-                                    <span className="text-rg">We design healthcare experiences that make information accessible, decisions easier, and journeys less stressful.</span> */}
-
-                                    {/* <span className="text-rg">Customers do&apos;t buy products. They buy experiences.</span>
-                                    <span className="text-rg">We build shopping journeys that reduce hesitation, increase conversions, and encourage repeat purchases.</span> */}
-
-                                    {/* <span className="text-rg">People invest in confidence before they invest in property.</span>
-                                    <span className="text-rg">We help real estate brands create digital experiences that build trust long before a site visit.</span> */}
-
-                                    {/* <span className="text-rg">The best learning experiences never feel complicated.</span>
-                                    <span className="text-rg">We create intuitive platforms that keep students focused on learning, not figuring out how things work.</span> */}
-
-                                    {/* <span className="text-rg">Innovation means little if people can&apos;t understand it.</span>
-                                    <span className="text-rg">We humanize emerging technologies through experiences that make complex products easier to adopt and trust.</span> */}
-
-                                    {/* <span className="text-rg">People remember how brands make them feel.</span>
-                                    <span className="text-rg">We help food and lifestyle brands create memorable identities that drive loyalty beyond the first purchase.</span> */}
                                 </p>
                             </div>
                         </div>
