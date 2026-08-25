3bced4127f22e9af8327f42449a7a467943ff954
rajvi16-rarepixels <rajvi.rarepixelsdesign@gmail.com>
2026-08-25 11:59:50 +0530

    fix 🐛:  fixed scroll to top button for mobile view

--- src/components/global/ScrollToTopButton.tsx
+++ src/components/global/ScrollToTopButton.tsx
@@ -2,56 +2,28 @@
 
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
 
 export default function ScrollToTopButton() {
 
   const [showButton, setShowButton] = useState(false);
 
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const firstSection = document.getElementById("first-section");
-
-  //     console.log("firstSection:", firstSection);
-
-  //     if (firstSection) {
-  //       // const firstSectionHeight = firstSection.offsetHeight;
-  //       const rect = firstSection.getBoundingClientRect();
-
-  //       setShowButton(rect.bottom < 0)
-
-  //       // setShowButton(window.scrollY > firstSectionHeight);
-  //     }
-  //   };
-
-  //   window.addEventListener("scroll", handleScroll);
-  //   return () => window.removeEventListener("scroll", handleScroll);
-  // }, []);
-  
-  const pathname = usePathname();
-
   useEffect(() => {
     const handleScroll = () => {
       const firstSection = document.getElementById("first-section");
 
+      console.log("firstSection:", firstSection);
+
       if (firstSection) {
-        const rect = firstSection.getBoundingClientRect();
-        setShowButton(rect.bottom < 0);
+        const firstSectionHeight = firstSection.offsetHeight;
+
+        setShowButton(window.scrollY > firstSectionHeight);
       }
     };
 
-    handleScroll();
-
     window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [pathname]);
-
-  useEffect(() => {
-  console.log("ScrollToTop mounted", pathname);
-}, [pathname]);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  
 
   const scrollToTop = () => {
     window.scrollTo({
