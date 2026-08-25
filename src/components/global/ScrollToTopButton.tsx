ec36a58502e0c6fd93430657515b214cea26f240
rare-aniket <aniket@rarepixelsdesign.com>
2026-08-25 15:20:14 +0530

    fix 🐛: Check the Score without video

--- src/components/global/ScrollToTopButton.tsx
+++ src/components/global/ScrollToTopButton.tsx
@@ -33,18 +33,13 @@ export default function ScrollToTopButton() {
   if (!showButton) return null;
 
   return (
-    <button
-      onClick={scrollToTop}
-      className={`  fixed bottom-6 right-6 z-50
-                    bg-primary text-white p-2
-                    flex items-center justify-center
-                    shadow-lg site-radius-10
-                    transition-transform transition-opacity
-                    duration-500 ease-out
-                    ${showButton
-          ? "translate-y-0 opacity-100"
-          : "translate-y-16 opacity-0 pointer-events-none"
-        }}`}>
+    <button onClick={scrollToTop} className={`fixed bottom-6 right-6 z-50
+                  bg-primary text-white p-2
+                  flex items-center justify-center
+                  shadow-lg site-radius-10
+                  transition-transform transition-opacity
+                  duration-500 ease-out
+                  ${showButton ? "translate-y-0 opacity-100" : "translate-y-16 opacity-0 pointer-events-none"}}`}>
       <Image className="back-top-pointer" src="/images/global/back-to-top.svg" alt="top-btn" width={24} height={24}></Image>
     </button>
   );
