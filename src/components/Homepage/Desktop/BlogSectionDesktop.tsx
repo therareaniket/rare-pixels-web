11d845a2ba63317eaa7aaaeb037375a07c640585
rajvi16-rarepixels <rajvi.rarepixelsdesign@gmail.com>
2026-09-21 15:21:08 +0530

    fix 🐛:  fixed home page services card hover animation for development card

--- src/components/Homepage/Desktop/BlogSectionDesktop.tsx
+++ src/components/Homepage/Desktop/BlogSectionDesktop.tsx
@@ -63,12 +63,11 @@ export default function BlogSectionDesktop() {
                                 modules={[Autoplay]}
                                 spaceBetween={30}
                                 slidesPerView={1.5}
-                                loop={true}
+                                // loop={true}
                                 speed={2200}
                                 autoplay={{ delay: 4000 }}
                                 breakpoints={{
-                                    0: { slidesPerView: 1.2, spaceBetween: 20 },
-                                    768: { slidesPerView: 1.4, spaceBetween: 20 },
+                                    0: { slidesPerView: 1.3, spaceBetween: 20 },
                                 }}>
                                 <SwiperSlide>
                                     <div className="hm-blog-card-left">
@@ -99,80 +98,6 @@ export default function BlogSectionDesktop() {
                                                 <Link title="Share on Twitter" href="#" className="icon-twitter text-dark-grey"></Link>
 
                                                 <Link title="Share on YouTube" href="#" className="icon-youtube text-dark-grey"></Link>
-                                            </div>
-                                        </div>
-
-                                        <p className="text-18 text-rg ">Two years of ideas turning into brands. Two years of challenges turning into opportunities. Two years of helping businesses become easier to notice, trust, and remember. <Link href="#" title="go to blog" className="text-primary">Read More</Link></p>
-                                    </div>
-                                </SwiperSlide>
-
-                                <SwiperSlide>
-                                    <div className="blog-card-right-3">
-                                        <div className="blog-card-3-left site-radius-20">
-                                            <Image src={`${CDN_URL}/images/homepage/blog/blog-image-2.png`} alt="blog-image" width={350} height={426}></Image>
-
-                                            <div className="blog-category-tag">
-                                                <p className="text-16 text-sb text-white">UI/UX Design</p>
-                                            </div>
-                                        </div>
-
-                                        <div className="blog-card-3-right">
-                                            <h3 className="text-sb blog-desk-title">Why Serious Businesses Invest in UX Masters, Not Just Creative Designers</h3>
-
-                                            <div className="hm-blog-author-name">
-                                                <p className="text-dark-grey text-18 text-rg"><span>By Bina Yogesh</span> | <span>May 29, 2026</span> | <span>7 mins read</span></p>
-                                            </div>
-
-                                            <div className="hm-blog-social-icons">
-                                                <Link href="#" title="Share on LinkedIn" className="icon-linkedin text-dark-grey"></Link>
-
-                                                <Link href="#" title="Share on Instagram" className="icon-instagram text-dark-grey"></Link>
-
-                                                <Link href="#" title="Share on Threads" className="icon-threads text-dark-grey"></Link>
-
-                                                <Link href="#" title="Share on Facebook" className="icon-facebook text-dark-grey"></Link>
-
-                                                <Link href="#" title="Share on Twitter" className="icon-twitter text-dark-grey"></Link>
-
-                                                <Link href="#" title="Share on YouTube" className="icon-youtube text-dark-grey"></Link>
-                                            </div>
-
-                                            <div className="hm-blog-read-detail">
-                                                <p className="text-18 text-rg">A visually attractive interface may convince users to explore your platform once. But whether they stay, engage, and return depends entirely on experience. <Link href="#" title="go to blog" className="text-primary">Read More</Link></p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </SwiperSlide>
-
-                                <SwiperSlide>
-                                    <div className="hm-blog-card-left">
-                                        <div className="hm-blog-image site-radius-20">
-                                            <Image src={`${CDN_URL}/images/homepage/blog/blog-image-1.png`} alt="blog-image" width={785} height={442}></Image>
-
-                                            <div className="blog-category-tag">
-                                                <p className="text-16 text-sb text-white">Life at RPD</p>
-                                            </div>
-                                        </div>
-
-                                        <h3 className="text-sb blog-desk-title">RarePixels Turns 2. The Big Game Is Still Loading.</h3>
-
-                                        <div className="hm-blog-author-icon-wrapper">
-                                            <div className="hm-blog-author">
-                                                <p className="text-18 text-rg text-dark-grey"><span>By Bina Yogesh</span> | <span>June 25, 2026</span> | <span>7 mins read</span></p>
-                                            </div>
-
-                                            <div className="hm-blog-social">
-                                                <Link href="#" title="Share on LinkedIn" className="icon-linkedin text-dark-grey"></Link>
-
-                                                <Link href="#" title="Share on Instagram" className="icon-instagram text-dark-grey"></Link>
-
-                                                <Link href="#" title="Share on Threads" className="icon-threads text-dark-grey"></Link>
-
-                                                <Link href="#" title="Share on Facebook" className="icon-facebook text-dark-grey"></Link>
-
-                                                <Link href="#" title="Share on Twitter" className="icon-twitter text-dark-grey"></Link>
-
-                                                <Link href="#" title="Share on YouTube" className="icon-youtube text-dark-grey"></Link>
                                             </div>
                                         </div>
 
