dd7c02c44c009339b11e490885bc8302cbd0bd55
rajvi16-rarepixels <rajvi.rarepixelsdesign@gmail.com>
2026-09-21 19:03:19 +0530

    fix 🐛: services page hero section responsive screen

--- src/components/Servicespage/ServicesHero.tsx
+++ src/components/Servicespage/ServicesHero.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 
 export default function ServicesHero() {
-    return(
+    return (
         <>
             <section id="first-section" className="services-hero-section section text-white" style={{ backgroundColor: "black" }}>
                 <div className="container">
                     <div className="serv-hero-wrapper">
                         <div className="serv-hero-contn">
                             <div className="serv-hero-ttl">
-                                <h1 className="text-sb">We Build <span className="serv-hero-span">Experiences </span> 
-                                That Businesses Choose First Worldwide</h1>
+                                <h1 className="text-sb">We Build <span className="serv-hero-span">Experiences </span>
+                                    That Businesses Choose First Worldwide</h1>
                             </div>
 
                             <div className="serv-hero-subttl">
@@ -18,20 +18,26 @@ export default function ServicesHero() {
 
                             <div className="serv-hero-stats">
                                 <div className="serv-stat">
-                                    <span className="h2 text-sb">100+ <Image src="/images/servicespage/herosection/serv-brand-svg.svg" alt="growth-graph" width={32} height={32}/></span>
+                                    <span className="h2 text-sb">100+ <Image src="/images/servicespage/herosection/serv-brand-svg.svg" alt="growth-graph" width={32} height={32} /></span>
 
                                     <p className="text-18 text-md">Brands Transformed</p>
                                 </div>
 
                                 <div className="serv-stat">
-                                    <span className="h2 text-sb">50+ <Image src="/images/servicespage/herosection/serv-clock-svg.svg" alt="time-clock" width={32} height={32}/></span>
-                                    
+                                    <span className="h2 text-sb">50+ <Image src="/images/servicespage/herosection/serv-clock-svg.svg" alt="time-clock" width={32} height={32} /></span>
+
                                     <p className="text-18 text-md">Bespoke Digital Products Engineered</p>
                                 </div>
                             </div>
                         </div>
 
                         <div className="serv-hero-vid"></div>
+                    </div>
+                </div>
+
+                <div className="services-strip-animation bg-light-black">
+                    <div className="services-strip bg-light-black">
+                        <h3 className="text-sb">UXUI Design   *  Branding    *    Digital Marketing    *    Development    *    Strategy   *    UXUI Design    *    Branding    *    Digital Marketing    *    Development *    Strategy   *    UXUI Design    *    Branding    *    Digital Marketing    *    Development *    Strategy   *    UXUI Design    *    Branding    *    Digital Marketing    *    Development * UXUI Design   *  Branding    *    Digital Marketing    *    Development    *    Strategy   * </h3>
                     </div>
                 </div>
             </section>
